5db5cb9c45625612f9a3172830750c8f6f24a050
michareichmann <micha.reichmann@gmail.com>
2019-12-04 10:15:17 +0100

    adding config and starting script for cmspixel

--- user/cmspixel/misc/starting_scripts/02_eudet_tlu_telescope.sh
+++ user/cmspixel/misc/starting_scripts/02_eudet_tlu_telescope.sh
@@ -23,15 +23,15 @@ xterm -T "Data Collector TLU" -e 'euCliCollector -n TriggerIDSyncDataCollector -
 sleep 1
 
 # Start TLU Producer
-xterm -T "EUDET TLU Producer" -e 'euCliProducer -n EudetTluProducer -t eudet_tlu -r tcp://${RUNCONTROLIP}:${RPCPORT}' & 
+xterm -T "EUDET TLU Producer" -e "euCliProducer -n EudetTluProducer -t eudet_tlu -r tcp://${RUNCONTROLIP}:${RPCPORT}" &
 
 # Start NI Producer locally connect to LV via TCP/IP
 xterm -T "NI/Mimosa Producer" -e 'euCliProducer -n NiProducer -t ni_mimosa -r tcp://${NIIP}:${RPCPORT}' &
 sleep 1
 
 # Start CMSPixel Producer
-xterm -T "CMSPixelProducer" -e 'euCliProducer -n CMSPixelProducerREF -t CMSREF -r tcp://${NIIP}:${RPCPORT}' &
-sleep 1
+#xterm -T "CMSPixelProducer" -e 'euCliProducer -n CMSPixelProducerREF -t CMSREF -r tcp://:${RPCPORT}' &
+#sleep 1
 
 # OnlineMonitor
 xterm -T "Online Monitor" -e 'StdEventMonitor -t StdEventMonitor -r tcp://${RUNCONTROLIP}' & 
