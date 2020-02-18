836ddbfe520261f870e50f7eed80befa59f094ee
michareichmann <micha.reichmann@gmail.com>
2020-02-18 16:58:21 +0100

    make tree proteus compatible

--- main/lib/ttreeConverter/include/TTreeFileWriter.hh
+++ main/lib/ttreeConverter/include/TTreeFileWriter.hh
@@ -12,9 +12,9 @@ namespace eudaq{
     TTreeFileWriter(std::string  out, const std::string & in);
     ~TTreeFileWriter() override;
     void WriteEvent(EventSPC ev) override;
-    void SetBranchAddresses();
-    void ClearVectors();
+    void SetBranches();
     void InitVectors();
+    void InitTrees();
     void FillVectors(const StandardEventSPC& stdev);
     static uint8_t FindNCMSPixels(const EventSPC& ev);
 
@@ -22,23 +22,23 @@ namespace eudaq{
     std::string m_filepattern;
     uint32_t m_run_n;
     TFile * m_tfile;
-    TTree * m_ttree;
+    TTree * m_event_tree;
+    std::vector<TTree*> m_plane_trees;
     ConfigurationSPC m_config;
-    uint8_t m_n_planes;
-    uint8_t m_n_cms_pixels;
+    uint8_t m_n_planes, m_n_telescope_planes, m_n_cms_pixels;
     bool m_init_vectors;
+    uint16_t m_max_hits;
 
     /** Branches */
     uint32_t b_event_nr;
-    double b_time_stamp_begin, b_time_stamp_end;
-    std::vector<uint16_t> b_n_hits;
+    uint64_t b_time_stamp_begin, b_time_stamp_end;
+    std::vector<int> b_n_hits;
+    std::vector<int*> b_column;
+    std::vector<int*> b_row;
+    std::vector<int*> b_adc;
+    std::vector<double*> b_charge;
     std::vector<uint16_t> b_trigger_phase;
     std::vector<uint16_t> b_trigger_count;
-    std::vector<std::vector<uint16_t>> * b_column;
-    std::vector<std::vector<uint16_t>> * b_row;
-    std::vector<std::vector<uint16_t>> * b_adc;
-    std::vector<std::vector<float>> * b_charge;
-
   };
 }
 
--- main/lib/ttreeConverter/src/TTreeFileWriter.cc
+++ main/lib/ttreeConverter/src/TTreeFileWriter.cc
@@ -21,8 +21,8 @@ namespace eudaq {
     auto dummy11 = Factory<FileWriter>::Register<TTreeFileWriter, std::string&&, std::string&&>(cstr2hash("root"));
   }
 
-  TTreeFileWriter::TTreeFileWriter(string out, const string & in) : m_filepattern(move(out)), m_config(nullptr), m_n_planes(0), m_n_cms_pixels(0), m_init_vectors(false), b_time_stamp_begin(0),
-                                                                    b_time_stamp_end(0) {
+  TTreeFileWriter::TTreeFileWriter(string out, const string & in) : m_filepattern(move(out)), m_config(nullptr), m_n_planes(0), m_n_cms_pixels(0),
+                                                                    m_init_vectors(false), m_max_hits(500), b_time_stamp_begin(0), b_time_stamp_end(0) {
 
     m_run_n = stoi(in.substr(in.rfind('/') + 4, in.rfind('_') - in.rfind('/') - 4));
 	  string foutput(FileNamer(m_filepattern).Set('R', m_run_n));
@@ -31,8 +31,7 @@ namespace eudaq {
     gInterpreter->GenerateDictionary("vector<vector<Float_t> >;vector<vector<UShort_t> >");
     EUDAQ_INFO("Preparing the outputfile: " + foutput);
     m_tfile = new TFile(foutput.c_str(), "RECREATE");
-    m_ttree = new TTree("tree","Converted from .raw");
-    SetBranchAddresses();
+    m_event_tree = new TTree("Event", "Event Information");
   }
 
   TTreeFileWriter::~TTreeFileWriter () {
@@ -64,55 +63,45 @@ namespace eudaq {
       if (stdev->NumPlanes() > 0){
         m_n_planes = stdev->NumPlanes();
         m_n_cms_pixels = FindNCMSPixels(ev);
+        m_n_telescope_planes = m_n_planes - m_n_cms_pixels;
+        InitTrees();
         InitVectors();
+        SetBranches();
       }
     }
     FillVectors(stdev);
     if (m_init_vectors){
-      m_ttree->Fill();
-      ClearVectors();
+      m_event_tree->Fill();
     }
   }
 
-  void TTreeFileWriter::SetBranchAddresses() {
-    
-    m_ttree->Branch("event_number", &b_event_nr, "event_number/i");
-    m_ttree->Branch("time_begin", &b_time_stamp_begin, "time_begin/D");
-    m_ttree->Branch("time_end", &b_time_stamp_end, "time_end/D");
-    m_ttree->Branch("n_hits", &b_n_hits);
-    m_ttree->Branch("trigger_phase", &b_trigger_phase);
-    m_ttree->Branch("trigger_count", &b_trigger_count);
+  void TTreeFileWriter::SetBranches() {
 
-    // telescope
-    m_ttree->Branch("column", &b_column);
-    m_ttree->Branch("row", &b_row);
-    m_ttree->Branch("adc", &b_adc);
-    m_ttree->Branch("charge", &b_charge);
-//
-//    // tu
-//    if (hasTU){
-//      m_ttree->Branch("beam_current", &f_beam_current, "beam_current/s");
-//      m_ttree->Branch("rate", &v_scaler);
-//    }
-  }
-
-  void TTreeFileWriter::ClearVectors() {
-
-    for (uint8_t i(0); i < m_n_planes; i++){
-      b_column->at(i).clear();
-      b_row->at(i).clear();
-      b_adc->at(i).clear();
-      b_charge->at(i).clear();
+    for (size_t i_plane(0); i_plane < m_n_planes; i_plane++){
+      m_plane_trees.at(i_plane)->Branch("NHits", &b_n_hits.at(i_plane), "NHits/I");
+      m_plane_trees.at(i_plane)->Branch("PixX", b_column.at(i_plane), "PixX[NHits]/I");
+      m_plane_trees.at(i_plane)->Branch("PixY", b_row.at(i_plane), "PixY[NHits]/I");
+      m_plane_trees.at(i_plane)->Branch("Value", b_adc.at(i_plane), "Value[NHits]/I");
+      if (i_plane >= m_n_telescope_planes){
+        m_plane_trees.at(i_plane)->Branch("TriggerPhase", &b_trigger_phase.at(i_plane - m_n_telescope_planes), "TriggerPhase/s");
+        m_plane_trees.at(i_plane)->Branch("TriggerCount", &b_trigger_count.at(i_plane - m_n_telescope_planes), "TriggerCount/s");
+      }
     }
+    m_event_tree->Branch("EventNumber", &b_event_nr, "EventNumber/i");
+    m_event_tree->Branch("TimeStamp", &b_time_stamp_begin, "TimeStamp/l");
+    m_event_tree->Branch("TimeBegin", &b_time_stamp_begin, "TimeBegin/l");
+    m_event_tree->Branch("TimeEnd", &b_time_stamp_end, "TimeEnd/l");
   }
 
   void TTreeFileWriter::InitVectors() {
 
     b_n_hits.resize(m_n_planes);
-    b_column = new vector<vector<uint16_t>>(m_n_planes);
-    b_row = new vector<vector<uint16_t>>(m_n_planes);
-    b_adc = new vector<vector<uint16_t>>(m_n_planes);
-    b_charge = new vector<vector<float>>(m_n_planes);
+    b_column.resize(m_n_planes, new int[m_max_hits]);
+    b_row.resize(m_n_planes, new int[m_max_hits]);
+    b_adc.resize(m_n_planes, new int[m_max_hits]);
+    b_charge.resize(m_n_planes, new double[m_max_hits]);
+    b_trigger_phase.resize(m_n_cms_pixels);
+    b_trigger_count.resize(m_n_cms_pixels);
     m_init_vectors = true;
   }
 
@@ -123,13 +112,16 @@ namespace eudaq {
     for (size_t i(0); i < stdev->NumPlanes(); i++){
       const auto & plane = stdev->GetPlane(i);
       b_n_hits.at(i) = plane.HitPixels();
+      uint16_t j(0);
       for (size_t i_frame(0); i_frame < plane.NumFrames(); i_frame++)
         for (size_t i_hit(0); i_hit < plane.HitPixels(i_frame); i_hit++){
-          b_column->at(i).push_back(plane.GetX(i_hit, i_frame));
-          b_row->at(i).push_back(plane.GetY(i_hit, i_frame));
-          b_adc->at(i).push_back(plane.GetPixel(i_hit, i_frame));
-          b_charge->at(i).push_back(plane.GetCharge(i_hit, i_frame));
+          b_column.at(i)[j] = uint16_t(plane.GetX(i_hit, i_frame));
+          b_row.at(i)[j] = uint16_t(plane.GetY(i_hit, i_frame));
+          b_adc.at(i)[j] = uint16_t(plane.GetPixel(i_hit, i_frame));
+//          b_charge.at(i)[j] = uint16_t(plane.GetCharge(i_hit, i_frame));
+          j++;
         }
+      m_plane_trees.at(i)->Fill();
     }
   }
 
@@ -140,5 +132,15 @@ namespace eudaq {
       if (sev->GetDescription().find("CMS") != string::npos)
         n++;
     return n;
+  }
+
+  void TTreeFileWriter::InitTrees() {
+
+    for (size_t i(0); i < m_n_planes; i++){
+      m_tfile->cd();
+      auto t_dir = m_tfile->mkdir(("Plane" + to_string(i)).c_str());
+      t_dir->cd();
+      m_plane_trees.push_back(new TTree("Hits", "Hit Information"));
+    }
   }
 }
